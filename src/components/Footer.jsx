967babe686b4025b68ffd9e4598274aa937d9812
TutCode222 <tuddycarraway@gmail.com>
2025-08-08 16:10:18 -0500

    chore: clear starter components (clean Example UI)

--- src/components/Footer.jsx
+++
@@ -1,7 +0,0 @@
-export default function Footer() {
-  return (
-    <footer className="footer">
-      <small>© {new Date().getFullYear()} Lewis Benson.</small>
-    </footer>
-  );
-}
--- src/components/Header.jsx
+++
@@ -1,7 +0,0 @@
-export default function Header() {
-  return (
-    <header className="header">
-      <h1 className="brand">React Starter</h1>
-    </header>
-  );
-}
